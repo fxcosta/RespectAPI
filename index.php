2857fcaa5bb4e961326a91016ca44abe6c146a89
fxcosta <fx3costa>
2015-10-06 00:55:16 -0300

    configuração com o banco de dados e uso do relational com sucesso!

--- index.php
+++ index.php
@@ -8,9 +8,16 @@ use Respect\Relational\Mapper;
 use Respect\Data\Collections\Collection;
 
 $config = new Container("config.ini");
+
+//$mapper = new Mapper(new PDO('mysql:host=127.0.0.1;port=3306;dbname=restbeer', 'root', 'root'));
 $mapper = new Mapper(new PDO($config->dsn, $config->user, $config->pass));
 $router = new Router();
 
 $router->any('/testing', function() {
    return 'Respect, bitch!';
+});
+
+$router->any('/mapper', function() use ($mapper) {
+    $result = $mapper->testing(['id' => 1])->fetchAll();
+    return $result[0]->name;
 });
